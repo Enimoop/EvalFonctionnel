5818c641480f10a8205552f2fd39b709c1af6483
Enimoop <marine.forcioli@laposte.net>
2024-05-15 15:08:43 +0200

    Add class Livre with method afficherDetails

---
+++ JAVA/EvalPoo/src/Livre.java
@@ -0,0 +1,57 @@
+
+public class Livre {
+	public String titre;
+	public String auteur;
+	public String isbn;
+	public boolean disponible;
+	
+	public Livre(String titre, String auteur, String isbn, boolean disponible) {
+		this.titre = titre;
+		this.auteur = auteur;
+		this.isbn = isbn;
+		this.disponible = disponible;
+	}
+	
+	public void afficherDetails() {
+		System.out.println("Titre : " + this.titre);
+		System.out.println("Auteur : " + this.auteur);
+		System.out.println("isbn : " + this.isbn);
+		System.out.println("disponible : " + this.disponible);
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+	
+	
+	
+}
